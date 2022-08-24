8a602c1a937d6f7196ecb47b14e963f1ef5911ab
Nina Ihde <ninaihde@ymail.com>
2022-08-24 23:21:00 +0200

    Use random seq length in classification

--- src/classify.py
+++ src/classify.py
@@ -7,43 +7,57 @@ import time
 import torch
 
 from model import Bottleneck, ResNet
+from numpy import random
 from ont_fast5_api.fast5_interface import get_fast5_file
 from scipy import stats
 
 
-def get_raw_data(file, reads, reads_ids, cutoff, seq_len, cut_after):
+def get_raw_data(file, reads, reads_ids, seq_lengths, cutoff, random_gen, min_seq_len, max_seq_len, cut_after):
     with get_fast5_file(file, mode='r') as f5:
         for read in f5.get_reads():
+            # get raw and scaled read
             raw_data = read.get_raw_data(scale=True)
+
             if len(raw_data) >= (cutoff + seq_len):
+                # get random sequence length per read
+                seq_len = random_gen.integers(min_seq_len, max_seq_len + 1)
+
+                seq_lengths.append(seq_len)
+                reads_ids.append(read.read_id)
+
                 if cut_after:
                     reads.append(raw_data[cutoff:])
                 else:
                     reads.append(raw_data[cutoff:(cutoff + seq_len)])
-                reads_ids.append(read.read_id)
-    return reads, reads_ids
+
+    return reads, reads_ids, seq_lengths
 
 
-def normalize(reads, batch_idx):
-    # normalize using z-score with median absolute deviation
-    m = np.median(reads, axis=1)
-    mad = stats.median_abs_deviation(reads, axis=1, scale='normal')
-    reads = ((reads - np.expand_dims(m, axis=1)) * 1.0) / (1.4826 * np.expand_dims(mad, axis=1))
+def normalize(data, batch_idx):
+    extreme_signals = list()
 
-    # replace extreme signals (modified absolute z-score larger than 3.5) with average of closest neighbors
-    # see Iglewicz and Hoaglin (https://hwbdocuments.env.nm.gov/Los%20Alamos%20National%20Labs/TA%2054/11587.pdf)
-    # x[0] indicates read and x[1] signal in read
-    x = np.where(np.abs(reads) > 3.5)
-    for i in range(x[0].shape[0]):
-        if x[1][i] == 0:
-            reads[x[0][i], x[1][i]] = reads[x[0][i], x[1][i] + 1]
-        elif x[1][i] == (reads[x[0][i]] - 1):
-            reads[x[0][i], x[1][i]] = reads[x[0][i], x[1][i] - 1]
+    for r_i, read in enumerate(data):
+        # normalize using z-score with median absolute deviation
+        median = np.median(read)
+        mad = stats.median_abs_deviation(read, scale='normal')
+        data[r_i] = list((read - median) / (1.4826 * mad))
+
+        # get extreme signals (modified absolute z-score larger than 3.5)
+        # see Iglewicz and Hoaglin (https://hwbdocuments.env.nm.gov/Los%20Alamos%20National%20Labs/TA%2054/11587.pdf)
+        extreme_signals += [(r_i, s_i) for s_i, signal_is_extreme in enumerate(np.abs(data[r_i]) > 3.5)
+                            if signal_is_extreme]
+
+    # replace extreme signals with average of closest neighbors
+    for read_idx, signal_idx in extreme_signals:
+        if signal_idx == 0:
+            data[read_idx][signal_idx] = data[read_idx][signal_idx + 1]
+        elif signal_idx == (len(data[read_idx]) - 1):
+            data[read_idx][signal_idx] = data[read_idx][signal_idx - 1]
         else:
-            reads[x[0][i], x[1][i]] = (reads[x[0][i], x[1][i] - 1] + reads[x[0][i], x[1][i] + 1]) / 2
+            data[read_idx][signal_idx] = (data[read_idx][signal_idx - 1] + data[read_idx][signal_idx + 1]) / 2
 
     print(f'[Step 2] Done data normalization with batch {str(batch_idx)}')
-    return torch.tensor(reads).float()
+    return torch.tensor(data).float()
 
 
 def process(reads, read_ids, batch_idx, bmodel, outpath, device):
@@ -64,13 +78,20 @@ def process(reads, read_ids, batch_idx, bmodel, outpath, device):
 @click.option('--model', '-m', help='path to pre-trained model', type=click.Path(exists=True))
 @click.option('--inpath', '-i', help='path to folder with input fast5 data', type=click.Path(exists=True))
 @click.option('--outpath', '-o', help='output result folder path', type=click.Path())
-@click.option('--seq_len', '-s', default=3000, help='number of raw signals used per read')
+@click.option('--min_seq_len', '-min', default=2000, help='minimum number of raw signals (after cutoff) used per read')
+@click.option('--max_seq_len', '-max', default=3000, help='maximum number of raw signals (after cutoff) used per read')
 @click.option('--cut_after', '-a', default=False,
               help='whether random sequence length per read is applied before or after normalization')
-@click.option('--batch_size', '-b', default=1, help='batch size')  # test data is sorted by time, so should be representative
+@click.option('--batch_size', '-b', default=1, help='batch size')  # test data is sorted by time, so 1 should be representative
+@click.option('--random_seed', '-s', default=42, help='seed for random operations')
 @click.option('--cutoff', '-c', default=1000, help='cutoff the first c signals')
-def main(model, inpath, outpath, seq_len, cut_after, batch_size, cutoff):
+def main(model, inpath, outpath, min_seq_len, max_seq_len, cut_after, batch_size, random_seed, cutoff):
     start_time = time.time()
+
+    if min_seq_len >= max_seq_len:
+        raise ValueError('The minimum sequence length must be smaller than the maximum sequence length!')
+
+    random_gen = random.default_rng(random_seed)
 
     # set device
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -84,12 +105,14 @@ def main(model, inpath, outpath, seq_len, cut_after, batch_size, cutoff):
     bmodel.load_state_dict(torch.load(model, map_location=device))
     print('[Step 0] Done loading model')
 
-    reads = []
-    read_ids = []
+    reads = list()
+    read_ids = list()
+    seq_lengths = list()
     batch_idx = 0
+
     for file in glob.glob(inpath + '/*.fast5'):
-        # load pA signals and make all reads same-sized
-        reads, read_ids = get_raw_data(file, reads, read_ids, cutoff, seq_len, cut_after)
+        reads, read_ids, seq_lengths = \
+            get_raw_data(file, reads, read_ids, seq_lengths, cutoff, random_gen, min_seq_len, max_seq_len, cut_after)
 
         # ensure that cutoff has not removed all reads
         if len(reads) > 0:
@@ -98,9 +121,16 @@ def main(model, inpath, outpath, seq_len, cut_after, batch_size, cutoff):
             if batch_idx % batch_size == 0:
                 print(f'[Step 1] Done loading data with batch {str(batch_idx)}, '
                       f'Getting {str(len(reads))} of sequences')
+
                 reads = normalize(reads, batch_idx)
-                if cut_after:
-                    reads = [r[:seq_len] for r in reads]
+
+                for i, r in enumerate(reads):
+                    if cut_after:
+                        reads[i] = reads[i][:seq_lengths[i]]
+
+                    # pad with zeros until maximum sequence length
+                    reads[i] += [0] * (max_seq_len - len(reads[i]))
+
                 process(reads, read_ids, batch_idx, bmodel, outpath, device)
                 print(f'[Step 4] Done with batch {str(batch_idx)}\n')
 
@@ -108,6 +138,8 @@ def main(model, inpath, outpath, seq_len, cut_after, batch_size, cutoff):
                 reads = []
                 del read_ids
                 read_ids = []
+                del seq_lengths
+                seq_lengths = []
 
     print(f'[Step FINAL] --- {time.time() - start_time} seconds ---')
 
