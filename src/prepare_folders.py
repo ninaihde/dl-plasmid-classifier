60060d3638a643ed38f57426f7183169e4d767d7
Nina Ihde <ninaihde@ymail.com>
2022-11-23 18:41:14 +0100

    Enable flexible folder structure

--- src/prepare_folders.py
+++ src/prepare_folders.py
@@ -2,7 +2,7 @@
 PREPROCESSING STEP 2/3
 This script cleans the reference data of both classes. In addition, it splits the plasmid references it into training,
 validation and test data based on the Jaccard similarity score, as we want to generalize our approach for plasmids.
-Afterwards, prepare_tensors.py can be executed.
+Afterwards, the simulation can be executed which is followed by prepare_tensors.py.
 """
 
 import click
@@ -14,7 +14,6 @@ import time
 
 from Bio import SeqIO
 from numpy import random
-from varname import nameof
 
 
 def calculate_signatures(input_dir):
@@ -45,8 +44,8 @@ def clean_neg_references(ref_neg_dir, ref_pos_dir, clean_sim_threshold, ref_neg_
                 refs_to_remove.append((n_path, n_id))
                 break
 
-    print(f'{len(refs_to_remove)} of {len(ref_neg_sigs)} reads will be removed, i.e. {len(set(refs_to_remove))} of '
-          f'{len(set([t[0] for t in ref_neg_sigs]))} files are affected.')
+    print(f'{len(refs_to_remove)} of {len(ref_neg_sigs)} reads will be removed, i.e. '
+          f'{len(set([t[0] for t in refs_to_remove]))} of {len(set([t[0] for t in ref_neg_sigs]))} files are affected.')
 
     # move references to new directory by leaving out too similar records
     for fasta_file in glob.glob(f'{ref_neg_dir}/*.fasta'):
@@ -58,7 +57,7 @@ def clean_neg_references(ref_neg_dir, ref_pos_dir, clean_sim_threshold, ref_neg_
                         with open(f'{ref_neg_dir_cleaned}/{os.path.basename(fasta_file)}', 'w') as f_out:
                             SeqIO.write(record, f_out, 'fasta')
                     except IOError:
-                        # only append record if file already opened
+                        # if file is already opened, only append record
                         SeqIO.write(record, f_out, 'fasta')
 
 
@@ -95,9 +94,9 @@ def split_by_similarity(signatures, split_sim_threshold, random_gen):
             continue
 
         is_sim = False
-        for idx2 in range(idx + 1, len(signatures), 1):
+        for idx2 in range(idx + 1, len(signatures)):
             sim = signatures[idx][1].similarity(signatures[idx2][1], downsample=True)
-            if sim > split_sim_threshold:
+            if sim >= split_sim_threshold:
                 is_sim = True
                 if sim != 1.0:
                     if signatures[idx2][0] not in train:
@@ -117,70 +116,72 @@ def split_by_similarity(signatures, split_sim_threshold, random_gen):
     val = random_gen.choice(test_val, size=int(len(test_val) / 2), replace=False)
     test = [path for path in test_val if path not in val]
 
-    # TODO: 401 references missing
-    print(f'Training   dataset: {len(train)} / {len(signatures)}')
-    print(f'Validation dataset: {len(val)}   / {len(signatures)}')
-    print(f'Test       dataset: {len(test)}  / {len(signatures)}')
+    print(f'Skipped: {len(skip_list)}')  # 2.920
+    print(f'Skipped but already in train: {len(set(skip_list).intersection(train))}')  # 2.537
+
+    print(f'Training   dataset: {len(train)} / {len(signatures)}')  # 23.168
+    print(f'Validation dataset: {len(val)}   / {len(signatures)}')  # 5.409
+    print(f'Test       dataset: {len(test)}  / {len(signatures)}')  # 5.410
     return train, val, test
 
 
-def move_split_pos_refs(ds_paths, sim_dir, ds_name):
-    folder_path = f'{sim_dir}/{ds_name}_ref_pos'
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
-
-    for file_path in ds_paths:
-        shutil.copyfile(file_path, f'{folder_path}/{os.path.basename(file_path)}')
-
-    print(f'References belonging to {ds_name}_ref_pos were successfully moved.')
-
-
-def split_pos_references(ref_pos_dir_cleaned, split_sim_threshold, random_gen, sim_dir):
+def split_pos_references(ref_pos_dir_cleaned, split_sim_threshold, random_gen, train_dir, val_dir, test_dir):
     print('Splitting positive references according to sequence similarity score...')
 
     signatures = calculate_signatures(ref_pos_dir_cleaned)
     train, val, test = split_by_similarity(signatures, split_sim_threshold, random_gen)
 
-    for (ds_paths, ds_name) in [(train, nameof(train)), (val, nameof(val)), (test, nameof(test))]:
-        move_split_pos_refs(ds_paths, sim_dir, ds_name)
+    for (ds_files, ds_dir) in [(train, train_dir), (val, val_dir), (test, test_dir)]:
+        if not os.path.exists(ds_dir):
+            os.makedirs(ds_dir)
+
+        for file in ds_files:
+            shutil.copyfile(file, f'{ds_dir}/{os.path.basename(file)}')
+
+        print(f'References successfully moved to {os.path.basename(ds_dir)}.')
 
 
 @click.command()
-@click.option('--test_real_dir', '-td', type=click.Path(exists=True), required=True,
+@click.option('--test_real', '-t', type=click.Path(exists=True), required=True,
               help='directory containing real test data (this script uses .fasta references + classify.py uses .fast5')
-@click.option('--ref_pos_dir', '-pd', type=click.Path(exists=True), required=True,
+@click.option('--ref_pos', '-p', type=click.Path(exists=True), required=True,
               help='directory containing reference genomes (.fasta) for positive class')
-@click.option('--ref_neg_dir', '-nd', type=click.Path(exists=True), required=True,
+@click.option('--ref_neg', '-n', type=click.Path(exists=True), required=True,
               help='directory containing reference genomes (.fasta) for negative class')
-@click.option('--sim_dir', '-sd', type=click.Path(exists=True), required=True,
-              help='directory in which subfolders for splitted positive references will be created')
+@click.option('--train_ref_pos', '-train', type=click.Path(), required=True,
+              help='directory for train references of positive class')
+@click.option('--val_ref_pos', '-val', type=click.Path(), required=True,
+              help='directory for validation references of positive class')
+@click.option('--test_ref_pos', '-test', type=click.Path(), required=True,
+              help='directory for test references of positive class')
 @click.option('--clean_sim_threshold', '-ct', default=0.9,
               help='threshold for sequence similarity score taken in cleaning procedures of references')
 @click.option('--split_sim_threshold', '-st', default=0.4,
               help='threshold for sequence similarity score taken in splitting procedure of positive references')
 @click.option('--random_seed', '-s', default=42, help='seed for random operations like splitting datasets')
-def main(test_real_dir, ref_pos_dir, ref_neg_dir, sim_dir, clean_sim_threshold, split_sim_threshold, random_seed):
+def main(test_real, ref_pos, ref_neg, train_ref_pos, val_ref_pos, test_ref_pos, clean_sim_threshold,
+         split_sim_threshold, random_seed):
     start_time = time.time()
     random_gen = random.default_rng(random_seed)
 
     print('Preparing chromosomes...\n')
-    ref_neg_dir_cleaned = f'{ref_neg_dir}_cleaned'
-    if not os.path.exists(ref_neg_dir_cleaned):
-        os.makedirs(ref_neg_dir_cleaned)
+    ref_neg_cleaned = f'{ref_neg}_cleaned'
+    if not os.path.exists(ref_neg_cleaned):
+        os.makedirs(ref_neg_cleaned)
 
     # Note: clean negative before positive references to include potential real plasmids in positive references
-    clean_neg_references(ref_neg_dir, ref_pos_dir, clean_sim_threshold, ref_neg_dir_cleaned)
+    clean_neg_references(ref_neg, ref_pos, clean_sim_threshold, ref_neg_cleaned)
     print(f'Runtime passed: {time.time() - start_time} seconds\n')
 
     print('Preparing plasmids...\n')
-    ref_pos_dir_cleaned = f'{ref_pos_dir}_cleaned'
-    if not os.path.exists(ref_pos_dir_cleaned):
-        os.makedirs(ref_pos_dir_cleaned)
+    ref_pos_cleaned = f'{ref_pos}_cleaned'
+    if not os.path.exists(ref_pos_cleaned):
+        os.makedirs(ref_pos_cleaned)
 
-    clean_pos_references(test_real_dir, ref_pos_dir, clean_sim_threshold, ref_pos_dir_cleaned)
+    clean_pos_references(test_real, ref_pos, clean_sim_threshold, ref_pos_cleaned)
     print(f'Runtime passed: {time.time() - start_time} seconds\n')
 
-    split_pos_references(ref_pos_dir_cleaned, split_sim_threshold, random_gen, sim_dir)
+    split_pos_references(ref_pos_cleaned, split_sim_threshold, random_gen, train_ref_pos, val_ref_pos, test_ref_pos)
     print(f'Overall runtime: {time.time() - start_time} seconds')
 
 
