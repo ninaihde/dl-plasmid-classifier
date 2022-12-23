6bbdedc7d106b222189ec37b5dda5b0dafa7cc86
Nina Ihde <ninaihde@ymail.com>
2022-12-23 01:41:00 +0100

    Split merged file of negative class

--- src/prepare_normalization.py
+++ src/prepare_normalization.py
@@ -8,60 +8,83 @@ and test data. Finally, it removes all original simulation directories to reduce
 import click
 import glob
 import os
+import pandas as pd
 import shutil
 
 from numpy import random
+from ont_fast5_api.conversion_tools.fast5_subset import Fast5Filter
 from ont_fast5_api.conversion_tools.single_to_multi_fast5 import batch_convert_single_to_multi
+from ont_fast5_api.fast5_interface import get_fast5_file
 
 
-def merge_and_compress(sim_dir, class_type):
+def merge_and_compress(input_dir, class_type, batch_size, threads):
     print('Merge and compress simulated fast5 files...')
-    output_dir = f'{sim_dir}_merged'
-    for folder in glob.glob(f'{sim_dir}/{class_type}/*.fasta'):
+    output_dir = f'{input_dir}_merged'
+    for folder in glob.glob(f'{input_dir}/{class_type}/*.fasta'):
         print(f'Folder: {folder}')
         input_dir = f'{folder}/fast5'
         if len(os.listdir(folder)) != 0:
             batch_convert_single_to_multi(input_path=input_dir, output_folder=output_dir,
-                                          filename_base=class_type[:3], batch_size=5000, threads=32,
-                                          target_compression='gzip', recursive=False, follow_symlinks=False)
+                                          filename_base=class_type[:3], batch_size=batch_size, threads=threads,
+                                          recursive=False, follow_symlinks=False, target_compression='gzip')
     print(f'Number of files in {output_dir}: {len(glob.glob(f"{output_dir}/*.fast5"))}')
 
 
-def split_randomly(paths, train_percentage, val_percentage, random_gen):
-    train = random_gen.choice(paths, size=int(len(paths) * train_percentage), replace=False)
-    val = random_gen.choice([p for p in paths if p not in train], size=int(len(paths) * val_percentage), replace=False)
-    test = [p for p in paths if p not in train and p not in val]
+def extract_read_ids(input_dir):
+    print('Extract IDs from negative reads...')
+    read_ids = list()
 
-    print(f'Training dataset:   {len(train)} / {len(paths)}')
-    print(f'Validation dataset: {len(val)}   / {len(paths)}')
-    print(f'Test dataset:       {len(test)}  / {len(paths)}')
-    return train, val, test
+    for file in glob.glob(f'{input_dir}/*.fast5'):
+        with get_fast5_file(file, mode='r') as f5:
+            read_ids += f5.get_read_ids()
 
-
-def split_neg_reads(sim_neg, train_percentage, val_percentage, random_gen, train_sim_neg, val_sim_neg, test_sim_neg):
-    print('Randomly splitting negative reads according to given percentages...')
-
-    paths = glob.glob(f'{sim_neg}/*.fast5')
-    train, val, test = split_randomly(paths, train_percentage, val_percentage, random_gen)
-
-    for (ds_files, ds_dir) in [(train, train_sim_neg), (val, val_sim_neg), (test, test_sim_neg)]:
-        if not os.path.exists(ds_dir):
-            os.makedirs(ds_dir)
-
-        for file in ds_files:
-            shutil.copyfile(file, f'{ds_dir}/{os.path.basename(file)}')
-
-        print(f'Reads successfully moved to {os.path.basename(ds_dir)}.')
+    return read_ids
 
 
-def combine_folders(dir_out, dir_in_neg, dir_in_pos):
-    print(f'Combining files into {dir_out}...')
+def split_read_ids(read_ids, random_gen, train_percentage, val_percentage, input_dir):
+    print('Randomly splitting negative read IDs according to given percentages...')
+    train = random_gen.choice(read_ids, size=int(len(read_ids) * train_percentage), replace=False)
+    val = random_gen.choice([p for p in read_ids if p not in train], size=int(len(read_ids) * val_percentage), replace=False)
+    test = [p for p in read_ids if p not in train and p not in val]
 
-    if not os.path.exists(dir_out):
-        os.makedirs(dir_out)
+    print(f'Training dataset:   {len(train)} / {len(read_ids)}')
+    print(f'Validation dataset: {len(val)}   / {len(read_ids)}')
+    print(f'Test dataset:       {len(test)}  / {len(read_ids)}')
 
-    for file in glob.glob(f'{dir_in_neg}/*.fast5') + glob.glob(f'{dir_in_pos}/*.fast5'):
-        shutil.copyfile(file, f'{dir_out}/{os.path.basename(file)}')
+    train_df = pd.DataFrame({'read_id': train})
+    val_df = pd.DataFrame({'read_id': val})
+    test_df = pd.DataFrame({'read_id': test})
+
+    train_df.to_csv(f'{input_dir}/train_read_ids.csv', index=False, sep='\t')
+    val_df.to_csv(f'{input_dir}/val_read_ids.csv', index=False, sep='\t')
+    test_df.to_csv(f'{input_dir}/test_read_ids.csv', index=False, sep='\t')
+
+
+def split_files(input_dir, output_dir, ds_type, batch_size, threads):
+    print(f'Moving part of negative reads to folder {os.path.basename(output_dir)}...')
+    Fast5Filter(input_folder=input_dir, output_folder=output_dir, filename_base='neg',
+                read_list_file=f'{input_dir}/{ds_type}_read_ids.csv', batch_size=batch_size, threads=threads,
+                recursive=False, file_list_file=None, follow_symlinks=False, target_compression='gzip')
+
+
+def split_neg_reads(input_dir, train_percentage, val_percentage, random_gen, train_sim_neg, val_sim_neg, test_sim,
+                    batch_size, threads):
+    read_ids = extract_read_ids(input_dir)
+    split_read_ids(read_ids, random_gen, train_percentage, val_percentage, input_dir)
+
+    split_files(input_dir, train_sim_neg, 'train', batch_size, threads)
+    split_files(input_dir, val_sim_neg, 'val', batch_size, threads)
+    split_files(input_dir, test_sim, 'test', batch_size, threads)
+
+
+def move_files(output_dir, input_dir):
+    print('Moving all simulated testing files into same folder...')
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for file in glob.glob(f'{input_dir}/*.fast5'):
+        shutil.copyfile(file, f'{output_dir}/{os.path.basename(file)}')
 
 
 @click.command()
@@ -77,25 +100,25 @@ def combine_folders(dir_out, dir_in_neg, dir_in_pos):
               help='directory containing simulated validation data for positive class (.fast5)')
 @click.option('--test_sim', type=click.Path(), required=True,
               help='directory for simulated test data of both classes (.fast5)')
-@click.option('--test_sim_neg', type=click.Path(), required=True,
-              help='directory for simulated test data for negative class (.fast5)')
 @click.option('--test_sim_pos', type=click.Path(exists=True), required=True,
               help='directory containing simulated test data for positive class (.fast5)')
 @click.option('--random_seed', '-s', default=42, help='seed for random splitting')
-@click.option('--train_pct', '-t', default=0.8, help='splitting percentage for negative training reads')
-@click.option('--val_pct', '-v', default=0.1, help='splitting percentage for negative validation reads')
-def main(sim_neg, train_sim_neg, train_sim_pos, val_sim_neg, val_sim_pos, test_sim, test_sim_neg, test_sim_pos,
-         random_seed, train_pct, val_pct):
+@click.option('--batch_size', '-b', default=50000, help='number of reads per batch for file generation')
+@click.option('--threads', '-t', default=64, help='number of thread to use for fast5 merging and splitting')
+@click.option('--train_pct', '-tp', default=0.8, help='splitting percentage for negative training reads')
+@click.option('--val_pct', '-vp', default=0.1, help='splitting percentage for negative validation reads')
+def main(sim_neg, train_sim_neg, train_sim_pos, val_sim_neg, val_sim_pos, test_sim, test_sim_pos, random_seed,
+         batch_size, threads, train_pct, val_pct):
     # convert simulated single-fast5 files into compressed multi-fast5 files
-    merge_and_compress(sim_neg, 'negative')
-    merge_and_compress(train_sim_pos, 'positive')
-    merge_and_compress(val_sim_pos, 'positive')
-    merge_and_compress(test_sim_pos, 'positive')
+    merge_and_compress(sim_neg, 'negative', batch_size, threads)
+    merge_and_compress(train_sim_pos, 'positive', batch_size, threads)
+    merge_and_compress(val_sim_pos, 'positive', batch_size, threads)
+    merge_and_compress(test_sim_pos, 'positive', batch_size, threads)
 
     # split simulated reads of negative class and move simulated test data into one folder
     random_gen = random.default_rng(random_seed)
-    split_neg_reads(f'{sim_neg}_merged', train_pct, val_pct, random_gen, train_sim_neg, val_sim_neg, test_sim_neg)
-    combine_folders(test_sim, test_sim_neg, f'{test_sim_pos}_merged')
+    split_neg_reads(f'{sim_neg}_merged2', train_pct, val_pct, random_gen, train_sim_neg, val_sim_neg, test_sim,
+                    batch_size, threads)
 
     # remove all original simulation directories to reduce amount of stored files
     # TODO: compress and backup not used directories in share-dir beforehand?
