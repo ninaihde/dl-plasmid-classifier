c356e2f1982ebc5a2e4f0946b4509be7603a5d9d
Nina Ihde <ninaihde@ymail.com>
2023-02-11 16:04:10 +0100

    Evaluate logs

--- src/evaluation/evaluate_testing.py
+++ src/evaluation/evaluate_testing.py
@@ -8,7 +8,7 @@ import glob
 import os
 import pandas as pd
 
-from plot_helper import create_barplot_for_several_metrics, create_lineplot_per_max
+from evaluation_helper import create_barplot_for_several_metrics, create_lineplot_per_max, get_time_and_rss, get_max_gpu_usage
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, matthews_corrcoef, precision_score, \
     recall_score
 
@@ -18,89 +18,115 @@ from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, m
               required=True)
 @click.option('--input_logs', '-l', help='path to folder with .txt logs/ all prints', type=click.Path(exists=True),
               required=True)
-@click.option('--output_plots', '-op', help='path to folder where subfolder for created plots of run ID will be stored',
-              type=click.Path(exists=True), required=True)
-@click.option('--output_results', '-or', help='path to folder where calculated results will be stored',
-              type=click.Path(exists=True), required=True)
-@click.option('--prefix', '-p', help='prefix of data folders to evaluate', default='final')
-@click.option('--run_id', '-r', help='identifier of runs to be evaluated', required=True)  # e.g. 'balancedLoss'
-def main(input_data, input_logs, output_plots, output_results, prefix, run_id):
-    # create subdirectory for plots to be generated
-    plots_dir = f'{output_plots}/{run_id}'
-    if not os.path.exists(plots_dir):
-        os.makedirs(plots_dir)
-
+@click.option('--output_figures', '-f', help='folder in which subfolder for created figures of run ID will be stored',
+              type=click.Path(), required=True)
+@click.option('--output_results', '-r', help='path to folder where calculated results will be stored',
+              type=click.Path(), required=True)
+def main(input_data, input_logs, output_figures, output_results):
     metrics = pd.DataFrame(
-        columns=['Maximum Sequence Length', 'Cutting Method', 'Number of Epochs', 'TP', 'TN', 'FP', 'FN',
-                 'Accuracy', 'Balanced Accuracy', 'Precision', 'Recall', 'F1S', 'MCC'])
+        columns=['ID', 'Maximum Sequence Length', 'Dataset', 'Criterion', 'Epochs', 'TP', 'TN', 'FP', 'FN',
+                 'Accuracy', 'Balanced Accuracy', 'Precision', 'TPR (Sensitivity)', 'TNR (Specificity)', 'F1S', 'MCC'])
 
-    for config_folder in [cf for cf in os.listdir(input_data) if cf.startswith(prefix)]:
-        gt = pd.read_csv(f'{input_data}/{config_folder}/gt_test_labels.csv')
-        max_seq_len = int(config_folder.split('_')[1].replace('max', ''))
-        cutting_method = config_folder.split('_')[2].replace('cut', '')
+    # create directories for plots and CSVs to be generated
+    if not os.path.exists(output_figures):
+        os.makedirs(output_figures)
+    if not os.path.exists(output_results):
+        os.makedirs(output_results)
 
-        for test_folder in glob.glob(f'{input_data}/{config_folder}/classify_*_{run_id}/'):
-            n_epochs = int(test_folder.split(os.path.sep)[-3].split('_')[1].replace('epochs', ''))
-            pred_files = [i for i in glob.glob(f'{test_folder}*.csv')]
-            pred = pd.concat([pd.read_csv(f) for f in pred_files])
+    for result_folder in glob.glob(f'{input_data}/*/'):
+        run_name = os.path.basename(result_folder[:-1])
+        max_seq_len = run_name.split('_')[0].replace('max', '')
+        n_epochs = run_name.split('_')[1].replace('epochs', '')
+        dataset = run_name.split('_')[2]
+        criterion = run_name.split('_')[3]
 
-            # merge ground truth and predicted labels based on read ID
-            merged = pd.merge(gt, pred, left_on='Read ID', right_on='Read ID')
+        gt = pd.read_csv(f'{input_data}/max{max_seq_len}_gt_test_{dataset}_labels.csv')
+        pred_files = [i for i in glob.glob(f'{result_folder}/*.csv')]
+        pred = pd.concat([pd.read_csv(f) for f in pred_files])
 
-            # calculate performance metrics of current testing folder
-            metrics = pd.concat([metrics, pd.DataFrame([{
-                'Maximum Sequence Length': max_seq_len,
-                'Cutting Method': cutting_method,
-                'Number of Epochs': n_epochs,
-                'TP': len(merged[(merged['Predicted Label'] == 'plasmid') & (merged['GT Label'] == 'plasmid')]),
-                'TN': len(merged[(merged['Predicted Label'] == 'chr') & (merged['GT Label'] == 'chr')]),
-                'FP': len(merged[(merged['Predicted Label'] == 'plasmid') & (merged['GT Label'] == 'chr')]),
-                'FN': len(merged[(merged['Predicted Label'] == 'chr') & (merged['GT Label'] == 'plasmid')]),
-                'Accuracy': accuracy_score(merged['GT Label'], merged['Predicted Label']),
-                # (TP + TN) / (TP + TN + FP + FN)
-                'Balanced Accuracy': balanced_accuracy_score(merged['GT Label'], merged['Predicted Label']),
-                # (TPR + TNR) / 2
-                'Precision': precision_score(merged['GT Label'], merged['Predicted Label'], pos_label='plasmid'),
-                # TP / (TP + FP)
-                'Recall': recall_score(merged['GT Label'], merged['Predicted Label'], pos_label='plasmid'),
-                # TPR: TP / (TP + FN)
-                'F1S': f1_score(merged['GT Label'], merged['Predicted Label'], pos_label='plasmid'),
-                # harmonic mean between precision and recall
-                'MCC': matthews_corrcoef(merged['GT Label'], merged['Predicted Label']),
-            }])], ignore_index=True)
+        # merge ground truth and predicted labels based on read ID
+        merged = pd.merge(gt, pred, left_on='Read ID', right_on='Read ID')
 
-            # store merged dataframe for analysis of FN and FP
-            merged.to_csv(f'{test_folder}gt_and_pred_labels.csv', index=False)
+        # calculate performance metrics of current result folder
+        metrics = pd.concat([metrics, pd.DataFrame([{
+            'ID': run_name,
+            'Maximum Sequence Length': int(max_seq_len) * 1000,
+            'Dataset': dataset,
+            'Criterion': criterion,
+            'Epochs': n_epochs,
+            'TP': len(merged[(merged['Predicted Label'] == 'plasmid') & (merged['GT Label'] == 'plasmid')]),
+            'TN': len(merged[(merged['Predicted Label'] == 'chr') & (merged['GT Label'] == 'chr')]),
+            'FP': len(merged[(merged['Predicted Label'] == 'plasmid') & (merged['GT Label'] == 'chr')]),
+            'FN': len(merged[(merged['Predicted Label'] == 'chr') & (merged['GT Label'] == 'plasmid')]),
+            'Accuracy': accuracy_score(merged['GT Label'], merged['Predicted Label']),
+            # (TP + TN) / (TP + TN + FP + FN)
+            'Balanced Accuracy': balanced_accuracy_score(merged['GT Label'], merged['Predicted Label']),
+            # (TPR + TNR) / 2
+            'Precision': precision_score(merged['GT Label'], merged['Predicted Label'], pos_label='plasmid'),
+            # TP / (TP + FP)
+            'TPR (Sensitivity)': recall_score(merged['GT Label'], merged['Predicted Label'], pos_label='plasmid'),
+            # Recall/ TPR: TP / (TP + FN)
+            'TNR (Specificity)': recall_score(merged['GT Label'], merged['Predicted Label'], pos_label='chr'),
+            # TNR: TN / (TN + FP)
+            'F1S': f1_score(merged['GT Label'], merged['Predicted Label'], pos_label='plasmid'),
+            # harmonic mean between precision and recall
+            'MCC': matthews_corrcoef(merged['GT Label'], merged['Predicted Label']),
+        }])], ignore_index=True)
+
+        # store merged dataframe
+        merged.to_csv(f'{output_results}/OUR_gt_and_pred_labels_{run_name}.csv', index=False)
 
     # add missing metrics and store testing results
     metrics['TNR'] = metrics['TN'] / (metrics['TN'] + metrics['FP'])  # specificity
     metrics['FPR'] = metrics['FP'] / (metrics['FP'] + metrics['TN'])  # 1 - specificity
     metrics['FNR'] = metrics['FN'] / (metrics['FN'] + metrics['TP'])
-    metrics['ID'] = 'max' + metrics['Maximum Sequence Length'].astype(int).astype(str) \
-                    + '_cut' + metrics['Cutting Method'] \
-                    + '_ep' + metrics['Number of Epochs'].astype(str)
-    metrics.to_csv(f'{output_results}/testing_results_{run_id}.csv', index=False)
+    metrics.to_csv(f'{output_results}/OUR_metrics.csv', index=False)
 
     # create plots with 4 subplots each, each subplot showing the testing results with respect to one metric
-    metric_groups = [['Balanced Accuracy', 'Precision', 'MCC', 'F1S'], ['Recall', 'TNR', 'FPR', 'FNR']]
-    create_barplot_for_several_metrics(metrics, metric_groups, plots_dir, 'test')
+    metric_groups = [['Balanced Accuracy', 'Precision', 'MCC', 'F1S'],
+                     ['TPR (Sensitivity)', 'TNR (Specificity)', 'FPR', 'FNR']]
+    create_barplot_for_several_metrics(metrics, metric_groups, output_figures, 'test', 'Dataset', 'OUR')
 
     # plot metrics per maximum sequence length
-    for metric in ['Balanced Accuracy', 'Accuracy', 'F1S', 'Precision', 'Recall', 'FPR', 'TNR', 'FNR', 'MCC']:
-        create_lineplot_per_max(metrics, metric, plots_dir, 'test')
+    for metric in ['Balanced Accuracy', 'Accuracy', 'F1S', 'Precision', 'TPR (Sensitivity)', 'FPR', 'TNR (Specificity)',
+                   'FNR', 'MCC']:
+        create_lineplot_per_max(metrics, metric, output_figures, 'test', 'Dataset', 'Criterion', 'OUR')
 
-    # extract and plot runtimes
-    runtimes = pd.DataFrame(columns=['Maximum Sequence Length', 'Cutting Method', 'Number of Epochs', 'Runtime (min)'])
-    for filepath in glob.glob(f'{input_logs}/classify_*_{run_id}.txt'):
-        last_line = open(filepath, 'r').readlines()[-1]
-        filename_splitted = os.path.basename(filepath).split('_')
-        runtimes = pd.concat([runtimes,
-                              pd.DataFrame([{'Maximum Sequence Length': int(filename_splitted[1].replace('max', '')),
-                                             'Cutting Method': filename_splitted[2].replace('cut', ''),
-                                             'Number of Epochs': int(filename_splitted[3].replace('epochs', '')),
-                                             'Runtime (min)': float(last_line.split(' ')[3]) / 60}])],
-                             ignore_index=True)
-    create_lineplot_per_max(runtimes, 'Runtime (min)', plots_dir, 'test')
+    # plot memory consumption (peak RSS, GPU) and different times
+    times_and_memory = pd.DataFrame(columns=['ID', 'Maximum Sequence Length', 'Dataset', 'Criterion', 'User Time',
+                                             'System Time', 'Elapsed Time', 'Max RSS (GB)', 'Max GPU Usage (GiB)'])
+    for log_file in glob.glob(f'{input_logs}/step7_infer_*.txt'):
+        if log_file.endswith('_gpu.txt'):
+            continue
+        else:
+            time_file = log_file
+            nvidia_file = log_file.replace('.txt', '_gpu.txt')
+
+            mx = log_file.split('_')[2].replace('b', '')
+            ds = log_file.split('_')[3]
+            ct = log_file.split('_')[4]
+
+            user_time, system_time, elapsed_time, max_rss = get_time_and_rss(time_file)
+            max_gpu_usage = get_max_gpu_usage(nvidia_file, 'python')
+
+            times_and_memory = pd.concat([times_and_memory,
+                                          pd.DataFrame([{'ID': f'{mx}_{ds}_{ct}',
+                                                         'Maximum Sequence Length': int(mx) * 1000,
+                                                         'Dataset': ds,
+                                                         'Criterion': ct,
+                                                         'User Time': user_time,
+                                                         'System Time': system_time,
+                                                         'Elapsed Time': elapsed_time,
+                                                         'Max RSS (GB)': max_rss,
+                                                         'Max GPU Usage (GiB)': max_gpu_usage}])],
+                                         ignore_index=True)
+    times_and_memory.to_csv(f'{output_results}/OUR_times_and_memory.csv', index=False)
+
+    create_lineplot_per_max(times_and_memory, 'User Time', output_figures, 'test', 'Dataset', 'Criterion', 'OUR')
+    create_lineplot_per_max(times_and_memory, 'System Time', output_figures, 'test', 'Dataset', 'Criterion', 'OUR')
+    create_lineplot_per_max(times_and_memory, 'Elapsed Time', output_figures, 'test', 'Dataset', 'Criterion', 'OUR')
+    create_lineplot_per_max(times_and_memory, 'Max RSS (GB)', output_figures, 'test', 'Dataset', 'Criterion', 'OUR')
+    create_lineplot_per_max(times_and_memory, 'Max GPU Usage (GiB)', output_figures, 'test', 'Dataset', 'Criterion', 'OUR')
 
     print('Finished.')
 
--- src/evaluation/evaluation_helper.py
+++ src/evaluation/evaluation_helper.py
@@ -1,9 +1,44 @@
 """
-This script offers functions used by both, evaluate_training.py and evaluate_testing.py.
+This script offers helper functions used by the evaluation scripts.
 """
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import time
+
+
+def get_time_and_rss(logfile):
+    with open(logfile) as f:
+        for line in f:
+            if 'User time' in line:
+                user_time = line.split(': ')[1].strip()  # seconds
+                user_time = time.strftime('%H:%M:%S', time.gmtime(float(user_time)))  # h:mm:ss
+            elif 'System time' in line:
+                system_time = line.split(': ')[1].strip()  # seconds
+                system_time = time.strftime('%H:%M:%S', time.gmtime(float(system_time)))  # h:mm:ss
+            elif 'Elapsed (wall clock) time' in line:
+                elapsed_time = line.split(': ')[1].strip()  # h:mm:ss or m:ss
+            elif 'Maximum resident set size' in line:
+                max_rss = line.split(': ')[1].strip()  # KB
+                max_rss = int(max_rss) / 1e+6  # GB
+
+    return user_time, system_time, elapsed_time, max_rss
+
+
+def get_max_gpu_usage(logfile, process_name):
+    gpu_usages = list()
+    with open(logfile) as f:
+        for line in f:
+            if process_name in line:
+                gpu_usages.append(line.split(process_name)[1].split('|')[0].strip())
+
+    for i, usage in enumerate(gpu_usages):
+        if 'MiB' not in usage:
+            raise ValueError('Not all GPU measurements are MiB values!')
+        else:
+            gpu_usages[i] = int(usage.split('MiB')[0]) / 1024  # GiB
+
+    return max(gpu_usages)
 
 
 def create_lineplot_for_single_metric_twice(data, measure, mx, cut, epochs, plots_dir):
@@ -53,9 +88,10 @@ def create_lineplot_for_single_metric(data, measure, mx, cut, epochs, plots_dir)
     plt.close()
 
 
-def create_barplot_for_several_metrics(data, metric_collections, plots_dir, type_id):
+def create_barplot_for_several_metrics(data, metric_collections, plots_dir, type_id, hue, prefix):
     plotdata = data.copy()
-    plotdata = plotdata.replace(['cutAfter_', 'cutBefore_'], ['', ''], regex=True)
+    for h in data[hue].unique():
+        plotdata['ID'] = plotdata['ID'].replace(f'_{h}', '', regex=True)
     plotdata = plotdata.sort_values(by='ID')
 
     for metric_collection in metric_collections:
@@ -68,7 +104,7 @@ def create_barplot_for_several_metrics(data, metric_collections, plots_dir, type
             sns.barplot(data=plotdata,
                         x='ID',
                         y=metric,
-                        hue='Cutting Method',
+                        hue=hue,
                         palette=sns.color_palette('colorblind'),
                         ax=ax[i])
 
@@ -82,40 +118,40 @@ def create_barplot_for_several_metrics(data, metric_collections, plots_dir, type
             # adjust text of subplot
             ax[i].set_title(metric, fontsize=22)
             ax[i].set_xlabel('', fontsize=18)
+            ax[i].tick_params('x', labelrotation=90)
             ax[i].set_ylabel(metric, fontsize=18)
             ax[i].tick_params(axis='x', labelsize=14)
             ax[i].tick_params(axis='y', labelsize=14)
 
         plt.suptitle(f'Evaluation of {"Validation" if type_id == "val" else "Test"} Dataset', fontsize=24)
         handles, labels = ax[0].get_legend_handles_labels()
-        plt.figlegend(handles, labels, title='Cutting Method', ncol=2, fontsize=14, loc='center',
+        plt.figlegend(handles, labels, title=hue, ncol=2, fontsize=14, loc='center',
                       bbox_to_anchor=(0.5, 0.9))
 
         plt.tight_layout(pad=6.0)
-        plt.savefig(f'{plots_dir}/{"_".join(metric_collection)}_{type_id}.png', dpi=300, facecolor='white')
+        plt.savefig(f'{plots_dir}/{prefix}_{"_".join(metric_collection)}_{hue}_{type_id}.png', dpi=300, facecolor='white')
         plt.close()
 
 
-def create_lineplot_per_max(data, metric, plots_dir, type_id):
+def create_lineplot_per_max(data, metric, plots_dir, type_id, hue, style, prefix):
     plotdata = data.copy()
     fig, ax = plt.subplots(figsize=(10, 8))
     sns.lineplot(data=plotdata,
                  x='Maximum Sequence Length',
                  y=metric,
-                 hue='Cutting Method',
-                 style='Number of Epochs')
+                 hue=hue,
+                 style=style)
 
     # adjust text
     plt.title(f'{metric.replace(" (min)", "")} per Maximum Sequence Length', fontsize=22)
     plt.xlabel('Maximum Sequence Length', fontsize=18)
     plt.ylabel(metric, fontsize=18)
-    plt.xticks(fontsize=14, ticks=plotdata['Maximum Sequence Length'],
-               labels=plotdata['Maximum Sequence Length'].astype(int).astype(str) + '000')  # spell out max_seq_length
+    plt.xticks(fontsize=14)
     plt.locator_params(axis='x', nbins=plotdata['Maximum Sequence Length'].nunique())
     plt.yticks(fontsize=14)
     plt.rcParams['legend.title_fontsize'] = 14
     ax.legend(fontsize=14, loc='upper left', bbox_to_anchor=(1, 1))
 
     plt.tight_layout()
-    plt.savefig(f'{plots_dir}/{metric.replace(" (min)", "")}_per_max_{type_id}.png', dpi=300, facecolor='white')
+    plt.savefig(f'{plots_dir}/{prefix}_{metric.replace(" ", "_")}_per_max_{hue}_{style}_{type_id}.png', dpi=300, facecolor='white')
     plt.close()
