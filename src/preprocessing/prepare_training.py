ac81bc66fcdc1f96fcac471c03bbb2f6cfe6ad31
Nina Ihde <ninaihde@ymail.com>
2023-02-21 16:47:13 +0100

    Separate test and train data prep

--- src/preprocessing/prepare_training.py
+++ src/preprocessing/prepare_training.py
@@ -2,8 +2,7 @@
 PREPROCESSING STEP 4/4
 This script normalizes all train and validation data using the z-score with the median absolute deviation. In
 addition, it performs cutting of the reads to a randomly chosen sequence length and padding of the reads to a fixed
-length called max_seq_len. Finally, it saves the train and validation data as torch tensors. For the testing datasets,
-only storing of the ground truth labels is performed.
+length called max_seq_len. Finally, it saves the train and validation data as torch tensors.
 """
 
 import click
@@ -54,12 +53,6 @@ def save_as_tensor(data, outpath_ds, batch_idx, use_single_batch=False):
 
 
 @click.command()
-@click.option('--test_real', type=click.Path(exists=True), required=True,
-              help='directory containing real test data (.fast5)')
-@click.option('--test_sim', type=click.Path(), required=True,
-              help='directory for simulated test data of both classes (.fast5)')
-@click.option('--test_sim_real', type=click.Path(), required=True,
-              help='directory for simulated real test data of both classes (.fast5)')
 @click.option('--train_sim_neg', type=click.Path(), required=True,
               help='directory for simulated train data for negative class (.fast5)')
 @click.option('--train_sim_pos', type=click.Path(exists=True), required=True,
@@ -77,7 +70,7 @@ def save_as_tensor(data, outpath_ds, batch_idx, use_single_batch=False):
               help='whether random sequence length per read of validation set is applied before or after normalization')
 @click.option('--random_seed', '-s', default=42, help='seed for random sequence length generation')
 @click.option('--batch_size', '-b', default=5000, help='amount of reads per batch, set to zero to use whole dataset size')
-def main(test_real, test_sim, test_sim_real, train_sim_neg, train_sim_pos, val_sim_neg, val_sim_pos, out_dir, cutoff,
+def main(train_sim_neg, train_sim_pos, val_sim_neg, val_sim_pos, out_dir, cutoff,
          min_seq_len, max_seq_len, cut_after, random_seed, batch_size):
     start_time = time.time()
     random_gen = random.default_rng(random_seed)
@@ -93,7 +86,7 @@ def main(test_real, test_sim, test_sim_real, train_sim_neg, train_sim_pos, val_s
     if batch_size == 0:
         use_single_batch = True
 
-    for ds_path in [test_real, test_sim, train_sim_neg, train_sim_pos, val_sim_neg, val_sim_pos]:
+    for ds_path in [train_sim_neg, train_sim_pos, val_sim_neg, val_sim_pos]:
         ds_name = os.path.basename(ds_path)
         print(f'\nDataset: {ds_name}')
 
@@ -110,8 +103,8 @@ def main(test_real, test_sim, test_sim_real, train_sim_neg, train_sim_pos, val_s
 
         batch_idx = 0
 
-        # create file for ground truth labels of validation and test datasets
-        if 'val' in ds_name or 'test' in ds_name:
+        # create file for ground truth labels of validation datasets
+        if 'val' in ds_name:
             label_df = pd.DataFrame(columns=['Read ID', 'GT Label'])
 
         for file in glob.glob(f'{ds_path}/*.fast5'):
@@ -119,15 +112,6 @@ def main(test_real, test_sim, test_sim_real, train_sim_neg, train_sim_pos, val_s
 
             with get_fast5_file(file, mode='r') as f5:
                 for read in f5.get_reads():
-                    # store ground truth labels for test dataset (label have to be in file name)
-                    if 'test' in ds_name:
-                        label = 'plasmid' if 'plasmid' in os.path.basename(file) \
-                                             or 'pos' in os.path.basename(file) else 'chr'
-                        label_df = pd.concat(
-                            [label_df, pd.DataFrame([{'Read ID': read.read_id, 'GT Label': label}])],
-                            ignore_index=True)
-                        continue
-
                     # get raw signals converted to pA values
                     raw_data = read.get_raw_data(scale=True)
 
@@ -173,32 +157,29 @@ def main(test_real, test_sim, test_sim_real, train_sim_neg, train_sim_pos, val_s
                             del seq_lengths
                             seq_lengths = list()
 
-        if 'test' not in ds_name:
-            print(f'Number of kept reads in dataset: {batch_idx}')
+        print(f'Number of kept reads in dataset: {batch_idx}')
 
-            # normalize if single batch is used and all files are processed
-            # or last batch did not reach batch size
-            if (use_single_batch and len(reads) > 0) or (not use_single_batch and len(reads) > 0):
-                reads = normalize(reads)
+        # normalize if single batch is used and all files are processed
+        # or last batch did not reach batch size
+        if (use_single_batch and len(reads) > 0) or (not use_single_batch and len(reads) > 0):
+            reads = normalize(reads)
 
-                for i in range(len(reads)):
-                    # apply random sequence length
-                    if cut_after and 'val' in ds_name:
-                        reads[i] = reads[i][:seq_lengths[i]]
+            for i in range(len(reads)):
+                # apply random sequence length
+                if cut_after and 'val' in ds_name:
+                    reads[i] = reads[i][:seq_lengths[i]]
 
-                    # pad with zeros until maximum sequence length
-                    reads[i] += [0] * (max_seq_len - len(reads[i]))
+                # pad with zeros until maximum sequence length
+                reads[i] += [0] * (max_seq_len - len(reads[i]))
 
-                save_as_tensor(reads, f'{out_dir}/{ds_name}', batch_idx, use_single_batch)
-                del reads
-                reads = list()
-                del seq_lengths
-                seq_lengths = list()
+            save_as_tensor(reads, f'{out_dir}/{ds_name}', batch_idx, use_single_batch)
+            del reads
+            reads = list()
+            del seq_lengths
+            seq_lengths = list()
 
         # store ground truth labels
-        if 'test' in ds_name:
-            label_df.to_csv(f'{out_dir}/max{max_seq_len}_gt_{ds_name}_labels.csv', index=False)
-        elif 'val' in ds_name:
+        if 'val' in ds_name:
             label_df.to_csv(f'{out_dir}/gt_val_{"pos" if "pos" in ds_name or "plasmid" in ds_name else "neg"}_labels.csv', index=False)
             with open(f'{out_dir}/val_{"pos" if "pos" in ds_name or "plasmid" in ds_name else "neg"}_read_ids.txt', 'w') as f:
                 for r_id in label_df['Read ID'].tolist():
